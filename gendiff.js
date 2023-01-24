dc0cdcb7ca78093bbd69c370232d862506d0b9cb
Rodion Gnezdilov <rodion.gnezdilov@x5.ru>
2023-01-24 21:17:43 +0300

    Fixed eslint errors

--- gendiff.js
+++ gendiff.js
@@ -1,66 +1,67 @@
 #!/usr/bin/env node
 const _ = require('lodash');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
-const {program} = require('commander');
+const { program } = require('commander');
 
 const templates = {
-    add: (key, value) => `  + ${key}: ${value}`,
-    remove: (key, value) => `  - ${key}: ${value}`,
-    noChanges: (key, value) => `    ${key}: ${value}`,
-}
+  add: (key, value) => `  + ${key}: ${value}`,
+  remove: (key, value) => `  - ${key}: ${value}`,
+  noChanges: (key, value) => `    ${key}: ${value}`,
+};
 
 const parseFile = (filename) => {
-    const file = fs.readFileSync(filename, 'utf8');
+  const file = fs.readFileSync(filename, 'utf8');
 
-    if (path.extname(filename) === '.json') {
-        return JSON.parse(file);
+  if (path.extname(filename) === '.json') {
+    return JSON.parse(file);
+  }
+
+  return {};
+};
+
+const genDiff = (filename1, filename2) => {
+  const obj1 = parseFile(filename1);
+  const obj2 = parseFile(filename2);
+
+  const keys = _.uniq([
+    ...Object.keys(obj1),
+    ...Object.keys(obj2),
+  ]);
+
+  const diff = _.sortBy(keys).flatMap((key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+
+    if (value1 === value2) {
+      return templates.noChanges(key, value1);
     }
 
-    return {};
-}
-const genDiff = (filename1, filename2) => {
-    const obj1 = parseFile(filename1);
-    const obj2 = parseFile(filename2);
+    if (!_.isUndefined(value1) && _.isUndefined(value2)) {
+      return templates.remove(key, value1);
+    }
 
-    let keys = _.uniq([
-        ...Object.keys(obj1),
-        ...Object.keys(obj2),
-    ]);
+    if (!_.isUndefined(value2) && _.isUndefined(value1)) {
+      return templates.add(key, value2);
+    }
 
-    const diff = _.sortBy(keys).flatMap((key) => {
-        const value1 = obj1[key];
-        const value2 = obj2[key];
+    return [templates.remove(key, value1), templates.add(key, value2)];
+  });
 
-        if (value1 === value2) {
-            return templates.noChanges(key, value1);
-        }
-
-        if (!_.isUndefined(value1) && _.isUndefined(value2)) {
-            return templates.remove(key, value1);
-        }
-
-        if (!_.isUndefined(value2) && _.isUndefined(value1)) {
-            return templates.add(key, value2);
-        }
-
-        return [templates.remove(key, value1), templates.add(key, value2)]
-    });
-
-    return `{\n${diff.join('\n')}\n}`;
-}
+  return `{\n${diff.join('\n')}\n}`;
+};
 
 program
-    .name('gendiff')
-    .arguments('filepath1')
-    .arguments('filepath2')
-    .description('Compares two configuration files and shows a difference.')
-    .action((filename1, filename2) => {
-        const diff = genDiff(filename1, filename2);
-        console.log(diff);
-    })
-    .version('1.0.0')
-    .option('-f, --format <type>', 'output format');
+  .name('gendiff')
+  .arguments('filepath1')
+  .arguments('filepath2')
+  .description('Compares two configuration files and shows a difference.')
+  .action((filename1, filename2) => {
+    const diff = genDiff(filename1, filename2);
+    console.log(diff);
+  })
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
