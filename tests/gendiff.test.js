e2d3d505834f98ae8d747027fd671dc089ed8bb9
Rodion Gnezdilov <rodion.gnezdilov@x5.ru>
2023-01-24 23:07:50 +0300

    Fixed eslint config

--- tests/gendiff.test.js
+++ tests/gendiff.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import genDiff from '../src/gendiff';
+import genDiff from '../src/gendiff.js';
 
 const filename1 = './tests/__fixtures__/file1.json';
 const filename2 = './tests/__fixtures__/file2.json';
