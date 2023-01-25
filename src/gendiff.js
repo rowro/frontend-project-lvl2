d99510928cf44739aa52034358b2f368ae5d86c2
Rodion Gnezdilov <rodion.gnezdilov@x5.ru>
2023-01-25 12:10:07 +0300

    Refactor diff

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,12 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 
-const templates = {
-  add: (key, value) => `  + ${key}: ${value}`,
-  remove: (key, value) => `  - ${key}: ${value}`,
-  noChanges: (key, value) => `    ${key}: ${value}`,
-};
-
 const parseFile = (filename) => {
   const file = fs.readFileSync(filename, 'utf8');
 
@@ -18,35 +12,52 @@ const parseFile = (filename) => {
   throw new Error('Only json files accepted');
 };
 
-const genDiff = (filename1, filename2) => {
-  const obj1 = parseFile(filename1);
-  const obj2 = parseFile(filename2);
-
+const findDiff = (obj1, obj2) => {
   const keys = _.uniq([
     ...Object.keys(obj1),
     ...Object.keys(obj2),
   ]);
 
-  const diff = _.sortBy(keys).flatMap((key) => {
+  return _.sortBy(keys).reduce((acc, key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
 
+    const plus = ['+', key, value2];
+    const minus = ['-', key, value1];
+    const noChanges = [' ', key, value1];
+
     if (value1 === value2) {
-      return templates.noChanges(key, value1);
+      return [...acc, noChanges];
     }
 
-    if (!_.isUndefined(value1) && _.isUndefined(value2)) {
-      return templates.remove(key, value1);
+    if (_.isUndefined(value1) || _.isUndefined(value2)) {
+      return [
+        ...acc,
+        _.isUndefined(value2) ? minus : plus,
+      ];
     }
 
-    if (!_.isUndefined(value2) && _.isUndefined(value1)) {
-      return templates.add(key, value2);
-    }
+    return [...acc, minus, plus];
+  }, []);
+};
 
-    return [templates.remove(key, value1), templates.add(key, value2)];
-  });
+const formatDiff = (diff) => {
+  if (!diff.length) {
+    return '{}';
+  }
 
-  return diff.length ? `{\n${diff.join('\n')}\n}` : '{}';
+  const content = diff.map(([char, key, value]) => `  ${char} ${key}: ${value}`).join('\n');
+
+  return `{\n${content}\n}`;
+};
+
+const genDiff = (filename1, filename2) => {
+  const obj1 = parseFile(filename1);
+  const obj2 = parseFile(filename2);
+
+  const diff = findDiff(obj1, obj2);
+
+  return formatDiff(diff);
 };
 
 export default genDiff;
