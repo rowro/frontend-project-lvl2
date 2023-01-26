83e5e5c0604f612c841cb5a1154aabf011097960
Rodion Gnezdilov <rodion.gnezdilov@x5.ru>
2023-01-26 17:39:06 +0300

    Refactor gendiff

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,55 +2,54 @@ import _ from 'lodash';
 import parseFile from './parsers.js';
 import stylish from './stylish.js';
 
+const getTree = (obj) => Object.keys(obj).map((key) => {
+  const value = obj[key];
+
+  if (_.isObject(value)) {
+    return { action: null, key, value: getTree(value) };
+  }
+
+  return { action: null, key, value };
+});
+
+const getNode = (action, key, value) => ({
+  action,
+  key,
+  value: _.isObject(value) ? getTree(value) : value,
+});
+
 const findDiff = (obj1, obj2) => {
   const keys = _.uniq([
     ...Object.keys(obj1),
     ...Object.keys(obj2),
   ]);
 
-  return _.sortBy(keys).reduce((acc, key) => {
+  return _.sortBy(keys).flatMap((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
-    const diffKeys = {
-      plus: `+ ${key}`,
-      minus: `- ${key}`,
-      noChanges: `  ${key}`,
-    };
+    const hasChanges = !_.isUndefined(value1) && !_.isUndefined(value2) && value1 !== value2;
 
-    if (!_.isUndefined(value1) && !_.isUndefined(value2) && value1 !== value2) {
+    if (hasChanges) {
       if (_.isObject(value1) && _.isObject(value2)) {
-        return {
-          ...acc,
-          [diffKeys.noChanges]: findDiff(value1, value2),
-        };
+        return { action: null, key, value: findDiff(value1, value2) };
       }
 
-      return {
-        ...acc,
-        [diffKeys.minus]: value1,
-        [diffKeys.plus]: value2,
-      };
+      return [
+        getNode('removed', key, value1),
+        getNode('added', key, value2),
+      ];
     }
 
-    if (_.isUndefined(value1) || _.isUndefined(value2)) {
-      if (_.isUndefined(value2)) {
-        return {
-          ...acc,
-          [diffKeys.minus]: value1,
-        };
-      }
-
-      return {
-        ...acc,
-        [diffKeys.plus]: value2,
-      };
+    if (_.isUndefined(value2)) {
+      return getNode('removed', key, value1);
     }
 
-    return {
-      ...acc,
-      [diffKeys.noChanges]: value1,
-    };
-  }, {});
+    if (_.isUndefined(value1)) {
+      return getNode('added', key, value2);
+    }
+
+    return getNode(null, key, value1);
+  });
 };
 
 const genDiff = (filename1, filename2, formatter = stylish) => {
--- src/stylish.js
+++ src/stylish.js
@@ -1,23 +1,24 @@
 import _ from 'lodash';
 
+const chars = {
+  added: '+',
+  removed: '-',
+  noChanges: ' ',
+};
+
 const stylish = (data, replacer = ' ', spacesCount = 4) => {
   if (_.isEmpty(data)) {
     return '{}';
   }
 
   const iter = (item, depth = 1) => {
-    if (_.isObject(item)) {
-      const values = Object.entries(item).map(([key, value]) => {
-        const hasLeadCharacterOrSpace = key.match(/[+-]/) || key.match(/\s\s/);
-        const spacerLength = hasLeadCharacterOrSpace ? (spacesCount * depth) - 2 : spacesCount * depth;
-        const spacer = replacer.repeat(spacerLength);
+    if (_.isArray(item)) {
+      const values = item.map(({ action, key, value }) => {
+        const spacer = replacer.repeat((spacesCount * depth) - 2);
         const content = iter(value, depth + 1);
+        const character = chars[action ?? 'noChanges'];
 
-        if (!content) {
-          return `${spacer}${key}:`;
-        }
-
-        return `${spacer}${key}: ${content}`;
+        return `${spacer}${character} ${key}: ${content}`.trimEnd();
       });
 
       return `{\n${values.join('\n')}\n${replacer.repeat(spacesCount * (depth - 1))}}`;
