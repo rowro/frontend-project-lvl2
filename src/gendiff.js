8fa5b287792fc7136d94a7c3525c780ab5373fa2
Rodion Gnezdilov <rodion.gnezdilov@x5.ru>
2023-01-26 21:45:23 +0300

    Added plain formatter

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,24 +1,27 @@
-import _ from 'lodash';
+import uniq from 'lodash/uniq';
+import sortBy from 'lodash/sortBy';
+import isObject from 'lodash/isObject';
+import isUndefined from 'lodash/isUndefined';
 import parseFile from './parsers.js';
 import getFormatter from './formatters/index.js';
 
 const getTree = (data) => {
-  if (!_.isObject(data)) {
+  if (!isObject(data)) {
     return data;
   }
 
   return Object.entries(data).map(([key, value]) => ({
     action: 'noChanges',
     key,
-    valueTo: _.isObject(value) ? getTree(value) : value,
+    valueTo: isObject(value) ? getTree(value) : value,
   }));
 };
 
 const getAction = (value1, value2) => {
-  const isAdded = _.isUndefined(value1);
-  const isRemoved = _.isUndefined(value2);
+  const isAdded = isUndefined(value1);
+  const isRemoved = isUndefined(value2);
   const isUpdated = !isRemoved && !isAdded && value1 !== value2;
-  const isDeepChanges = isUpdated && _.isObject(value1) && _.isObject(value2);
+  const isDeepChanges = isUpdated && isObject(value1) && isObject(value2);
 
   if (isDeepChanges) return 'deepChanges';
   if (isUpdated) return 'updated';
@@ -28,19 +31,19 @@ const getAction = (value1, value2) => {
 };
 
 const findDiff = (obj1, obj2) => {
-  const keys = _.uniq([
+  const keys = uniq([
     ...Object.keys(obj1),
     ...Object.keys(obj2),
   ]);
 
-  return _.sortBy(keys).flatMap((key) => {
+  return sortBy(keys).flatMap((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
 
     const action = getAction(value1, value2);
     const valueFrom = ['updated', 'removed'].includes(action) ? getTree(value1) : undefined;
-    let valueTo;
 
+    let valueTo;
     if (action === 'deepChanges') {
       valueTo = findDiff(value1, value2);
     } else if (action !== 'removed') {
